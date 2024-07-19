fbdf41b550dbfde33f5390f0e557e9bbd2720cb5
akolle <andykoller55@yahoo.com>
2024-07-18 20:20:06 -0400

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import people from './data';
 
 
 const App = () => {
-  const [index, setIndex] = useState(0);
-  
+  const [index, setIndex] = useState(1);
+  const {name, job, image, text} = people[index];
+
+  console.log(name);
+
   return <h2>Reviews Starter</h2>;
 };
 export default App;
